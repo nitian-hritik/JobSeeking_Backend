02ad78b5b1182aff4c62b7eec3a003c27f9b6eee
nitian-hritik <rajeshkumishra40@gmail.com>
2024-04-03 20:30:17 +0530

    third commit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,13 +4,13 @@ export const sendToken = (user, statusCode, res, message) => {
     maxAge: 24*60*60*1000,
     // for deployment
 
-    // sameSite:"none",
-    // secure:true,
-    // httpOnly: false, // Set httpOnly to true
+    sameSite:"none",
+    secure:true,
+    httpOnly: false, // Set httpOnly to true
 
     //for localhost
-    httpOnly:true,
-    secure:false,
+    // httpOnly:true,
+    // secure:false,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
